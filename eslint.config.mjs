2da566ff178e57bf13f9e4041a840f63233a7c55
Dmytro Polishchuk <dvp0777@icloud.com>
2024-04-12 15:27:45 -0400

    m

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,25 @@
 import globals from 'globals'
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
 
+
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 import pluginJs from '@eslint/js'
+import eslintPluginImport from 'eslint-plugin-import';
 
-// mimic CommonJS variables -- not needed if using CommonJS
+// Define your ESLint configuration
+const eslintConfig = {
+  // Your ESLint rules and configurations here
+  plugins: ['import'],
+  rules: {
+    // Specify rules provided by the 'import' plugin
+    'import/no-unresolved': 'error',
+    // Add more rules as needed
+  },
+};
+
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
